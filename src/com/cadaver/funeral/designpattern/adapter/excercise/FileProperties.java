1c612a729cab9844a267fd9e2fcc8b8d2222c708
mitsuo.yamazaki <fsrv.titan8@gmail.com>
2015-10-25 18:31:57 +0900

    #6 演習問題2-2の解答

--- src/com/cadaver/funeral/designpattern/adapter/excercise/FileProperties.java
+++ src/com/cadaver/funeral/designpattern/adapter/excercise/FileProperties.java
@@ -7,24 +7,16 @@ import java.util.Properties;
  * Created by mitsuo on 2015/10/25.
  */
 public class FileProperties extends Properties implements FileIO {
-    public void readFromFile(String filename) {
-        try {
-            load(new FileInputStream(filename));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void readFromFile(String filename) throws IOException {
+        load(new FileInputStream(filename));
     }
 
-    public void writeToFile(String filename) {
-        try {
-            store(new FileOutputStream(filename), "test");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void writeToFile(String filename) throws IOException{
+        store(new FileOutputStream(filename), "written by FileProperties");
     }
 
     public String getValue(String key) {
-        return getProperty(key);
+        return getProperty(key, "");
     }
 
     public void setValue(String key, String value) {
